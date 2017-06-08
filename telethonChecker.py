577d49e56b92e818d9b44dcb1c541ab68bb552be
Savinyh Alex <suineg@inbox.ru>
2017-06-08 08:01:50 +0500

    2

--- telethonChecker.py
+++ telethonChecker.py
@@ -2,10 +2,14 @@ from telethon import TelegramClient
 from telethon.tl.functions.auth import check_phone
 import time
 
+def write_phones(data):
+    with open('ckeck.txt', 'w', encoding='cp1251') as f:
+        f.write(data)
+
 api_id   = 143159
 api_hash = 'e1f978e828363b22a6bd7d482c540ca8'
-#phone    = '+79003845427'
-phone    = '+79226727926'
+phone    = '+79003845427'
+#phone    = '+79226727926'
 
 client = TelegramClient('+79226727926', api_id, api_hash)
 client.connect()
@@ -16,10 +20,18 @@ if not client.is_user_authorized():
     client.sign_in(phone, input('Enter the code: '))
 
 f = open('phones.txt', 'r')
-
+data = ''
+check_result = ''
 for phone in f:
-    print(phone.strip("\n") + ";" + str(client.invoke(check_phone.CheckPhoneRequest(phone)).phone_registered))
-    time(1000)
+    try:
+        check_result = str(client.invoke(check_phone.CheckPhoneRequest(phone)).phone_registered)
+    except Exception:
+        check_result = 'False'
+    data += phone.strip("\n") + ";" + check_result
+    print(phone.strip("\n") + ";" + check_result)
+    time.sleep(5)
+write_phones(data)
+print('***successes***')
 
 
 #client.disconnect()
